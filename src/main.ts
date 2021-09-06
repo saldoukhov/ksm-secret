d736bd0254675e4d814e9aabe974ad8369356316
Sergey Aldoukhov <saldoukhov@gmail.com>
2021-09-05 17:49:51 -0700

    init

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,39 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import * as sodium from 'tweetsodium'
+import {getSecrets, initializeStorage, inMemoryStorage} from '@keeper-security/secrets-manager-core'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    try {
+        // Prepare Github communications
+        const pa_token = core.getInput('accessToken')
+        core.getInput('accessToken')
+        const octokit = github.getOctokit(pa_token)
+        const getPublicKey = octokit.rest.actions['getRepoPublicKey']
+        const upsertSecret = octokit.rest.actions['createOrUpdateRepoSecret']
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+        // Convert one time token to KSM config
+        const hostAndToken = core.getInput('oneTimeToken').split(':')
+        const ksmConfig = {}
+        const storage = inMemoryStorage(ksmConfig)
+        await initializeStorage(storage, hostAndToken[1], hostAndToken[0]) // Initialize the config with the private key
+        await getSecrets({storage}) // Get the secrets once so the client is bound to the private key
+        const secret = JSON.stringify(ksmConfig)
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+        // Save the config to the repository
+        const {data: public_key} = await getPublicKey(github.context.repo)
+        const publicKeyBytes = Buffer.from(public_key.key, 'base64')
+        const secretBytes = sodium.seal(Buffer.from(secret), publicKeyBytes)
+        const secretValue = Buffer.from(secretBytes).toString('base64')
+        const {status} = await upsertSecret({
+            ...github.context.repo,
+            secret_name: core.getInput('configName'),
+            encrypted_value: secretValue,
+            key_id: public_key.key_id
+        })
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
 
 run()
